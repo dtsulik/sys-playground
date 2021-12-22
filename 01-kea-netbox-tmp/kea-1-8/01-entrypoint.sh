10dad164d26c8aa26886fb3fdce8c5f81844929e
David <dtsulik@gmail.com>
2021-12-23 00:38:57 +0400

    remove tmp kea

--- 01-kea-netbox-tmp/kea-1-8/01-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Initializing db"
-
-/usr/sbin/kea-admin db-init mysql -u root \
-    -p $MYSQL_ROOT_PASSWORD -n $MYSQL_DATABASE -h db
-
-/usr/sbin/kea-dhcp4 -c /etc/kea/kea-dhcp4.conf
--- 01-kea-netbox-tmp/shim/01-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./kea-netbox-shim.py -m db -p 8800 -u root \
-    -s $MYSQL_ROOT_PASSWORD -d $MYSQL_DATABASE -t $NETBOX_TOKEN \
-    -n $NETBOX_URL_WITHOUT_TRAILING_SLASH
